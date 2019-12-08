a320abe2337f91f5707e7820fa8f1e5ea8a83dc5
ZhangXi <zhangxi.work@foxmail.com>
2019-12-08 12:01:16 +0800

    init project

---
+++ src/main/java/com/github/whalerain/springbootkata/SpringbootKataApplication.java
@@ -0,0 +1,14 @@
+package com.github.whalerain.springbootkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+
+@SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
+public class SpringbootKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKataApplication.class, args);
+    }
+
+}
