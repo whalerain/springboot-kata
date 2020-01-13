ab1ff77909530173ccde969d084c95079ee53cbf
ZhangXi <zhangxi.work@foxmail.com>
2020-01-13 22:03:17 +0800

    add: 配置监听器与过滤器

---
+++ springboot-basic/src/main/java/com/github/whalerain/springbootkata/config/ListenerConfiguration.java
@@ -0,0 +1,23 @@
+package com.github.whalerain.springbootkata.config;
+
+import com.github.whalerain.springbootkata.bean.MyListener;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ZhangXi
+ */
+@Configuration
+public class ListenerConfiguration {
+
+    @Bean
+    public ServletListenerRegistrationBean<MyListener> MyListenerRegister(MyListener myListener) {
+        ServletListenerRegistrationBean<MyListener> bean = new ServletListenerRegistrationBean<>();
+        bean.setListener(myListener);
+        bean.setEnabled(true);
+        bean.setOrder(1);
+        return bean;
+    }
+
+}
