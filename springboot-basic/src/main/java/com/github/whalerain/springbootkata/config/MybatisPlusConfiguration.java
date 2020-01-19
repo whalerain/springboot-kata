4fc94540ee5dcdaaaf8f8178b9a62e2b313357a6
ZhangXi <zhangxi.work@foxmail.com>
2020-01-19 21:06:29 +0800

    refactor: 补充注释

--- springboot-basic/src/main/java/com/github/whalerain/springbootkata/config/MybatisPlusConfiguration.java
+++ springboot-basic/src/main/java/com/github/whalerain/springbootkata/config/MybatisPlusConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * Mybatis Plus配置
+ *
  * @author ZhangXi
  */
 @Configuration
